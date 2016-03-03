328dff525c9cb8d8606b9349bc197cd0bb0fdeaa
Amir Barylko <amir@barylko.com>
2016-03-03 13:06:58 -0600

    Added social media icons

--- src/elm/Views.elm
+++ src/elm/Views.elm
@@ -20,21 +20,27 @@ view address model =
     ]
 
 bannerMenu =
-  div [] [text "Winnipeg Dot Net User Group ponnies and rainbows"]
+  div [class "banner-menu"] [text "Winnipeg Dot Net User Group ponnies and rainbows"]
 
 socialMedia =
   let 
-    icon id = i [class <| "fa fa-" ++ id] []
-    linkTo link i t = a [href link, target "_blank"] [icon i, text t]
+    -- icon id = i [class <| "fa fa-2x fa-" ++ id] []
+    icon id =
+      span [class "fa-stack fa-2x"]
+        [ i [class "fa fa-circle fa-stack-2x"] []
+        , i [class <| "fa fa-stack-1x fa-" ++ id] []
+        ]
+      
+    linkTo link i t = a [class "sm-link", href link, target "_blank"] [icon i, text t]
     twitter  = "https://twitter.com/wpgnetug" 
     facebook = "https://www.facebook.com/winnipegdotnet"
     youTube  = "https://www.youtube.com/channel/UC6OzdI6-htXE_97zamJRaaA"
     gitHub   = "https://github.com/WpgDotNetUG/UserGroupWebsite"
   in 
   div [class "social-media"]
-    [ linkTo twitter  "twitter"      "Follow"
-    , linkTo facebook "facebook"     "Like"
-    , linkTo youTube  "youtube-play" "Subscribe"
-    , linkTo gitHub   "github"       "Fork"
+    [ linkTo twitter  "twitter"  "Follow"
+    , linkTo facebook "facebook" "Like"
+    , linkTo youTube  "play"     "Subscribe"
+    , linkTo gitHub   "github"   "Fork"
     ]
 
