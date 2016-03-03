8dc0680510ba5167dd4263ac3cdd2964cacb39a4
Amir Barylko <amir@barylko.com>
2016-03-03 08:10:38 -0600

    Added based structure to the view

--- src/Views.elm
+++ src/Views.elm
@@ -9,9 +9,32 @@ import Models exposing (..)
 
 view : Address Action -> Model -> Html
 view address model = 
-  div [class "outside-container"]
-    [ div [class "social-media"] [text "Social media"]
-    , div [class "wpg-header"] [text "Winnipeg .NET User Group"] 
-    , div [class "next-event"] [text "Next event goes here"] 
+  div [class "container"]
+    [ header [] [socialMedia, bannerMenu]
+    , section [class "next-event"] [text "Next event goes here"] 
+    , section [class "past-events"] [text "View all past events"]
+    , div [class "subscribe-list"] [text "subscribe & twitter stream"]
+    , section [class "sponsors"] [text "sponsors go heere"]
+    , section [class "contact-us"] [text "Amir, David & Roy"]
+    , footer [] [text "here goes the footer"]
+    ]
+
+bannerMenu =
+  div [] [text "Winnipeg Dot Net User Group ponnies and rainbows"]
+
+socialMedia =
+  let 
+    icon id = i [class <| "fa fa-" ++ id] []
+    linkTo link i t = a [href link, target "_blank"] [icon i, text t]
+    twitter  = "https://twitter.com/wpgnetug" 
+    facebook = "https://www.facebook.com/winnipegdotnet"
+    youTube  = "https://www.youtube.com/channel/UC6OzdI6-htXE_97zamJRaaA"
+    gitHub   = "https://github.com/WpgDotNetUG/UserGroupWebsite"
+  in 
+  div [class "social-media"]
+    [ linkTo twitter  "twitter"      "Follow"
+    , linkTo facebook "facebook"     "Like"
+    , linkTo youTube  "youtube-play" "Subscribe"
+    , linkTo gitHub   "github"       "Fork"
     ]
 
